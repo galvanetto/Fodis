a577f8b0bc997a3f4346e08fb5beff762fd7b1e9
nicolagalvanetto <nicola.galvanetto@gmail.com>
2017-05-26 13:55:04 +0200

    Commit safe

--- Fodis for Matlab users/Fodis1.0/Plot Function/gaussian1D.m
+++
@@ -1,8 +0,0 @@
-function val = gaussian1D(x, mu, sigma,warea)
-%  x     - Input vector
-%  mu    - Mean
-%  sigma - Standard deviation
-
-    % Evaluate a 1D gaussian.
-    val = (warea / (sigma * sqrt(2 * pi))) * exp(-(x - mu).^2 ./ (2 * sigma^2));
-end
